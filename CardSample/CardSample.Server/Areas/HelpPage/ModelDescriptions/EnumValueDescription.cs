2e6d4964f9ea272a1dab231d6e2c721505f67946
Md. Sabbir Ahamed <sabbiryan@gmail.com>
2018-10-05 20:34:31 +0600

    solution added initial commit

---
+++ CardSample/CardSample.Server/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace CardSample.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ CardSample/CardSample.Server/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CardSample.Server.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
